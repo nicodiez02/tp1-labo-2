43d857f14db59e491987b4b96295174c84e43dc8
Patricio Mc Allister <43398227+McPatto@users.noreply.github.com>
2023-10-03 16:55:25 -0300

    first commit

---
+++ primer-tp-labo.c
@@ -0,0 +1,10 @@
+// Este es el primer TP de labo II.
+// Sientanse libres de cambiar el nombre del archivo por el que querramos todos.
+// Les dejo una base para que arranquemos ;)
+// Configurense el VsCode para que formatee automaticamente los archivos al guardar. Asi estamos todos en la misma sintonia
+
+int main()
+{
+  // A Hacer Magia bbs
+  return 0;
+}
