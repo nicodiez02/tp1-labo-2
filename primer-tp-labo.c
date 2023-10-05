83ca77271ba64a5b25e4e11e554ebe79bbc76a4f
nicodiez02 <nicoo.diez02@gmail.com>
2023-10-04 21:23:52 -0300

    lista-de-opciones

--- primer-tp-labo.c
+++ primer-tp-labo.c
@@ -3,8 +3,58 @@
 // Les dejo una base para que arranquemos ;)
 // Configurense el VsCode para que formatee automaticamente los archivos al guardar. Asi estamos todos en la misma sintonia
 
-int main()
-{
-  // A Hacer Magia bbs
-  return 0;
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void procedaOperacion(char opcionElegida) {
+    printf("Opción elegida: %c\n", opcionElegida);
+    char opcionElegidaMinuscula = tolower(opcionElegida);
+
+    if(opcionElegidaMinuscula == 'a'){
+      printf("Testing a");
+
+    }else if(opcionElegidaMinuscula == 'b'){
+
+    }else if(opcionElegidaMinuscula == 'c'){
+
+    }else if(opcionElegidaMinuscula == 'd'){
+
+    }else if(opcionElegidaMinuscula == 'e'){
+
+    }else if(opcionElegidaMinuscula == 'f'){
+
+    }else if(opcionElegidaMinuscula == 'g'){
+
+    }else if(opcionElegidaMinuscula == 'h'){
+      
+    }else{
+      printf("------------------------- La opcion ingresada no es valida -------------------------\n");
+      obtenerSeleccion(&opcionElegida);
+    }
+}
+
+void obtenerSeleccion(char * opcionElegida) {
+    printf("¿Qué operación desea realizar?\n");
+    printf("A) Crear archivo binario\n"
+           "B) Listar dat\n"
+           "C) Alta\n"
+           "D) Buscar\n"
+           "E) Modificar\n"
+           "F) Baja lógica\n"
+           "G) Baja física\n"
+           "H) Listar xyz\n");
+
+    scanf(" %c", opcionElegida); 
+    procedaOperacion(*opcionElegida);
+
+}
+
+int main() {
+    char opcion;
+    char *opcionElegida = &opcion;
+
+    obtenerSeleccion(opcionElegida);
+
+    return 0;
 }
