e6a60b0e8ebe3dffbaedc427d324c0c0b4a6e1fa
nicodiez02 <nicoo.diez02@gmail.com>
2023-10-30 12:40:48 -0300

    mensajes-de-error

--- helpers/baja-logica/baja-logica.h
+++ helpers/baja-logica/baja-logica.h
@@ -1,8 +1,8 @@
 void mostrarPropiedad(propiedad prop) {
-                                printf("%.2d | %11s | %21s | %14s | %.1d | %.1d | %5f | %5f | %8f | %3s | %10s | %10s | %1s | %d\n",
-                                    prop.id, prop.fecha_de_ingreso, prop.zona, prop.ciudad, prop.dormitorios,
-                                    prop.banos, prop.superficie_total, prop.superficie_cubierta, prop.precio, prop.moneda,
-                                    prop.tipo, prop.operacion, prop.fecha_de_salida, prop.activo);
+    printf("%.2d | %11s | %21s | %14s | %.1d | %.1d | %5f | %5f | %8f | %3s | %10s | %10s | %1s | %d\n",
+        prop.id, prop.fecha_de_ingreso, prop.zona, prop.ciudad, prop.dormitorios,
+        prop.banos, prop.superficie_total, prop.superficie_cubierta, prop.precio, prop.moneda,
+        prop.tipo, prop.operacion, prop.fecha_de_salida, prop.activo);
                             
 }
 
@@ -20,7 +20,7 @@ bool idValido(FILE ** pArchivo){
                     fseek(*pArchivo, posicionFila, SEEK_SET);
                 }else{
                     idValido = false;
-                    printf("\n- - - - -  No existe un registro con ese id - - - - - \n");
+                    printf("\n- - - - -  NO EXISTE UN REGISTRO CON ESE ID - - - - - \n");
                 }
             }
 
@@ -43,58 +43,67 @@ void bajalogica(FILE * pArchivo){
 
     if(fread(&prop, sizeof(propiedad), 1, pArchivo) == 1){
         if(strlen(prop.fecha_de_salida) <= 1){
+            if(prop.activo != 0){
             
-            printf("¿Esta seguro que quiere dar de baja la siguiente fila?:\n");
-            mostrarPropiedad(prop);
-            posicionFila = ftell(pArchivo) - sizeof(propiedad);
-            while (seguirPreguntando){
-            
-                printf("\nPresione S o N:");
-                scanf("%1s", cadena);
+                printf("¿Esta seguro que quiere dar de baja la siguiente fila?:\n");
+                mostrarPropiedad(prop);
+                posicionFila = ftell(pArchivo) - sizeof(propiedad);
+                while (seguirPreguntando){
                 
-                if(strlen(cadena) == 1){
-                    eliminar = tolower(cadena[0]);
+                    printf("\nPresione S o N:");
+                    scanf("%1s", cadena);
                     
-                    if(eliminar == 's'){
-                        prop.activo = 0;
-                        fseek(pArchivo, posicionFila, SEEK_SET);
-                        fwrite(&prop, sizeof(propiedad), 1, pArchivo);            
-                        seguirPreguntando = false;
+                    if(strlen(cadena) == 1){
+                        eliminar = tolower(cadena[0]);
+                        
+                        if(eliminar == 's'){
+                            prop.activo = 0;
+                            fseek(pArchivo, posicionFila, SEEK_SET);
+                            fwrite(&prop, sizeof(propiedad), 1, pArchivo);            
+                            seguirPreguntando = false;
 
-                        printf("REGISTRO ACTUALIZADO DE FILAS ACTIVAS:\n");
-                        fseek(pArchivo, 0, SEEK_SET);
-                        fread(&prop, sizeof(propiedad), 1, pArchivo);
-      
-                        while (!feof(pArchivo))
-                        {
-                            if(prop.activo == 1){
-
-                                printf("%.2d | %11s | %21s | %14s | %.1d | %.1d | %5f | %5f | %8f | %3s | %10s | %10s | %1s | %d\n",
-                                prop.id, prop.fecha_de_ingreso, prop.zona, prop.ciudad, prop.dormitorios,
-                                prop.banos, prop.superficie_total, prop.superficie_cubierta, prop.precio, prop.moneda,
-                                prop.tipo, prop.operacion, prop.fecha_de_salida, prop.activo);
-                            }
-                            
+                            printf("REGISTRO ACTUALIZADO DE FILAS ACTIVAS:\n");
+                            fseek(pArchivo, 0, SEEK_SET);
                             fread(&prop, sizeof(propiedad), 1, pArchivo);
+        
+                            while (!feof(pArchivo))
+                            {
+                                if(prop.activo == 1){
+
+                                    printf("%.2d | %11s | %21s | %14s | %.1d | %.1d | %5f | %5f | %8f | %3s | %10s | %10s | %1s | %d\n",
+                                    prop.id, prop.fecha_de_ingreso, prop.zona, prop.ciudad, prop.dormitorios,
+                                    prop.banos, prop.superficie_total, prop.superficie_cubierta, prop.precio, prop.moneda,
+                                    prop.tipo, prop.operacion, prop.fecha_de_salida, prop.activo);
+                                }
+                                
+                                fread(&prop, sizeof(propiedad), 1, pArchivo);
+                            }
+
+                            
+                            
+
+                        }else if(eliminar == 'n'){
+                            seguirPreguntando = false;
+                        }else{
+                            printf("\n- - - - -  VERIFIQUE LO INGRESADO Y REINTÉNTELO - - - - - \n");
+
                         }
 
-                        
-                        
-
-                    }else if(eliminar == 'n'){
-                        seguirPreguntando = false;
                     }else{
-                        printf("Verifique lo ingresado y reintentelo.\n");
+                        printf("\n- - - - -  VERIFIQUE LO INGRESADO Y REINTÉNTELO - - - - - \n");
+
                     }
-
-                }else{
-                    printf("Verifique lo ingresado y reintentelo.\n");
+                    while (getchar() != '\n'); //Limpio buffer de entrada
                 }
-                while (getchar() != '\n'); //Limpio buffer de entrada
-            }
 
+            }else{
+                
+                printf("\n- - - - -  EL REGISTRO YA FUE DADO DE BAJA CON ANTERIORIDAD - - - - - \n");
+
+             }
         }else{
-            printf("La fila elegida no se puede eliminar porque su fecha de salida es no nula");
+            printf("\n- - - - -  LA FILA ELEGIDA NO SE PUEDE ELIMINAR PORQUE SU FECHA DE SALIDA ES NO NULA - - - - - \n");
+
         }
     }
 }
